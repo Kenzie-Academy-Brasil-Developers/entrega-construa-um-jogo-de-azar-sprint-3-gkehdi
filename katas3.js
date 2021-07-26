5af5d04793bb89b26497c42afb718a86d2f4a1f0
gkehdi <gkehdi@gmail.com>
2021-07-26 10:58:32 -0300

    jogoDeAzar

---
+++ katas3.js
@@ -0,0 +1,72 @@
+function jogar(){
+
+    return Math.floor(Math.random ()*3); 
+}
+const button = document.querySelector("#botao");
+const answer=document.querySelector("#answer");
+
+button.addEventListener("click", function(){
+let numAleatorio = jogar();
+let phrase = "";
+//alert(numAleatorio);
+
+if(numAleatorio === 0){
+    
+    phrase = "yes!";
+}
+if (numAleatorio === 1){
+
+    phrase = "No!"
+} 
+if (numAleatorio ===2){
+
+    phrase = "Depends on you!"
+
+}
+
+answer.innerHTML = phrase;
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let msg = document.createElement("p");
+// msg.setAttribute("src", "p");
+// document.getElementById("resposta").appendChild("msgum");
+// document.getElementById("resposta").innerHTML = ("oioioi!!!!");
+// break;
+
+
+// function iniciar() {
+
+//     let play = Math.floor(Math.random() * 10);
+    
+//     switch (play) {
+
+//         case 0:
+
+        // let msg = document.createElement("p");
+        // msg.setAttribute("src", "p");
+        // document.getElementById("resposta").appendChild("msgum");
+        // document.getElementById("resposta").innerHTML = ("oioioi!!!!");
+        // break;
+    //     let message = document.createElement("div");
+    //     message.id("message")
+    //     let answer = document.getElementById("message");
+    //     answer.appendChild(message);
+    //     answer.innerHTML("Você vai ser muito rico");
+    //     break;
+    // }
+
+// }
+// }
